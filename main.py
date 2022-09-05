65bad78293f396bd1868606482973b5607e476ba
Alvaro Valdelamar <alvaro22martelo@gmail.com>
2022-09-05 17:15:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from turtle import width
 import pygame
+import os
 
 # Creating game window
 WIDTH, HEIGHT = 500, 500 # WIDTH, HEIGHT of the screen window
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Frames per second to update game
+FPS = 60
 
 # Changing the window's name
 pygame.display.set_caption('Space Invaders')
 
 WHITE = (255, 255, 255) # White color in RGB tuple
 
+SPACESHIP_IMAGE = pygame.image.load('game_images\spaceship.png')
 
 def draw_window():
     # Filling the windows with a color RGB
@@ -20,10 +24,13 @@ def draw_window():
 
 
 def main():
-
+    # Control the speed of the while loop
+    clock = pygame.time.Clock()
     # Game loop that terminates when the games end
     run = True
     while run:
+        # Making sure the game runs at the set FPS
+        clock.tick(FPS)
 
         # Looping through events in the game
         for event in pygame.event.get():
