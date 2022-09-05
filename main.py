4e03bfa6ae46a997459d63ff9a207bed4853dfa3
Alvaro Valdelamar <alvaro22martelo@gmail.com>
2022-09-05 17:14:33 -0500

    Updated main.py and Created game_images folder

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import pygame
 WIDTH, HEIGHT = 500, 500 # WIDTH, HEIGHT of the screen window
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# Changing the window's name
+pygame.display.set_caption('Space Invaders')
+
+WHITE = (255, 255, 255) # White color in RGB tuple
+
+
+def draw_window():
+    # Filling the windows with a color RGB
+    WIN.fill(WHITE)
+
+    #Update the display in every run of the loop
+    pygame.display.update()
+
+
 def main():
 
     # Game loop that terminates when the games end
@@ -17,7 +31,8 @@ def main():
             # check if user quitted the game
             if event.type == pygame.QUIT:
                 run = False # Exit while loop
-            
+        
+        draw_window()
     
     pygame.quit() # End the game
 
