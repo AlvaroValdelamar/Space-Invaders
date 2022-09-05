bb78ddabec573a18b96573d237eafd74016b3f5f
Alvaro Valdelamar <alvaro22martelo@gmail.com>
2022-09-05 17:05:12 -0500

    Update main.py
    Created basic game window and game loop.

--- main.py
+++ main.py
@@ -2,5 +2,24 @@ from turtle import width
 import pygame
 
 # Creating game window
-WIDTH, HEIGHT = 700, 400 # WIDTH, HEIGHT of the screen window
+WIDTH, HEIGHT = 500, 500 # WIDTH, HEIGHT of the screen window
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+
+    # Game loop that terminates when the games end
+    run = True
+    while run:
+
+        # Looping through events in the game
+        for event in pygame.event.get():
+            
+            # check if user quitted the game
+            if event.type == pygame.QUIT:
+                run = False # Exit while loop
+            
+    
+    pygame.quit() # End the game
+
+if __name__ == "__main__":
+    main()
