80180d7c242c91a42273b251eadd329b704f7487
Alvaro Valdelamar <alvaro22martelo@gmail.com>
2022-09-05 19:16:06 -0500

    Update main.py
    Added row of aliens and updated events for collisions of user, aliens and bullets.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+pygame.font.init()
 
 # Creating game window
 WIDTH, HEIGHT = 500, 500 # WIDTH, HEIGHT of the screen window
@@ -18,6 +19,9 @@ MAX_BULLETS = 10
 
 # Changing the window's name
 pygame.display.set_caption('Space Invaders')
+
+# Health font for display
+HEALT_FONT = pygame.font.SysFont('comicsans', 40)
 
 WHITE = (255, 255, 255) # White color in RGB tuple
 PURPLE = (255, 0, 255)
@@ -45,14 +49,18 @@ BACKGROUND_IMAGE = pygame.image.load(os.path.join('game_images','background.png'
 BACKGROUND_IMAGE = pygame.transform.scale(BACKGROUND_IMAGE, (WIDTH, HEIGHT))
 
 
-def draw_window(user, alien, bullets):
+def draw_window(spaceship, aliens, bullets, spaceship_health):
     # Filling the windows with a color RGB
     WIN.fill(WHITE)
 
     # Drawing surfaces in the screen
     WIN.blit(BACKGROUND_IMAGE, (0, 0))
-    WIN.blit(SPACESHIP_IMAGE, (user.x, user.y))
-    WIN.blit(ALIEN_IMAGE, (alien.x, alien.y))
+    WIN.blit(SPACESHIP_IMAGE, (spaceship.x, spaceship.y))
+    for alien in aliens:
+        WIN.blit(ALIEN_IMAGE, (alien.x, alien.y))
+
+    health_text = HEALT_FONT.render("<3 "+str(spaceship_health), 1, WHITE)
+    WIN.blit(health_text, (WIDTH - health_text.get_width()-10, 10))
 
     for bullet in bullets:
         pygame.draw.rect(WIN, PURPLE, bullet)
@@ -73,17 +81,26 @@ def spaceship_movement(keys_pressed, spaceship):
     if keys_pressed[pygame.K_DOWN] and spaceship.y + spaceship.height < HEIGHT - VEL: # DOWN KEY
         spaceship.y += VEL
 
-def alien_movement(alien):
-    alien.y += 1
+# Moves aliens and checks they collided with the user spaceship
+def alien_movement(aliens, spaceship):
+    for alien in aliens:
+        alien.y += 1
+        if alien.colliderect(spaceship): # Check if a bullets hits an alien
+            pygame.event.post(pygame.event.Event(ALIEN_HIT))
+            aliens.remove(alien)
+        elif alien.y > HEIGHT:
+            aliens.remove(alien)
 
 # Moves bullets and check if the collided with an alien
-def handle_bullets(bullets, alien):
+def handle_bullets(bullets, aliens):
     for bullet in bullets:  # Loop through all bullets in the game
         bullet.y -= VEL_BULLET # Moving bullets
-        if alien.colliderect(bullet): # Check if a bullets hits an alien
-            pygame.event.post(pygame.event.Event(USER_HIT))
-            bullets.remove(bullet) # Remove bullet from list
-        elif bullet.y < 0:
+        for alien in aliens:
+            if alien.colliderect(bullet): # Check if a bullets hits an alien
+                pygame.event.post(pygame.event.Event(USER_HIT))
+                bullets.remove(bullet) # Remove bullet from list
+                aliens.remove(alien)
+        if bullet.y < 0:
             bullets.remove(bullet)
 
 
@@ -92,9 +109,14 @@ def main():
     # Rectangle to keep track of the user spaceship
     spaceship = pygame.Rect(300, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
     # Rectangle to keep track of the alien
-    alien = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    aliens = []
+    for x in range(8):
+        alien = pygame.Rect(60 * x, ALIEN_HEIGHT, ALIEN_WIDTH, ALIEN_HEIGHT)
+        aliens.append(alien)
     # Bullets fired by the user
     bullets = []
+
+    spaceship_health = 5
 
     # Control the speed of the while loop
     clock = pygame.time.Clock()
@@ -117,18 +139,29 @@ def main():
                     bullet = pygame.Rect(spaceship.x + spaceship.width//2,
                                         spaceship.y - spaceship.height//2, 10, 5)
                     bullets.append(bullet)
-        
-        handle_bullets(bullets, alien)
+
+            if event.type == USER_HIT:
+                pass
+
+            if event.type == ALIEN_HIT:
+                spaceship_health -= 1
+
+        if spaceship_health <= 0:
+            spaceship_health = 0
+            game_over_text = 'GAME OVER'
+
+        # Draw and check for bullet hits to aliens
+        handle_bullets(bullets, aliens)
 
         # Read keys pressed by the user
         keys_pressed = pygame.key.get_pressed()
         # Move the user spaceship
         spaceship_movement(keys_pressed, spaceship)
         # Move the alien
-        alien_movement(alien)
+        alien_movement(aliens, spaceship)
 
         # Update position of the player and aliens
-        draw_window(spaceship, alien, bullets)
+        draw_window(spaceship, aliens, bullets, spaceship_health)
     
     pygame.quit() # End the game
 
